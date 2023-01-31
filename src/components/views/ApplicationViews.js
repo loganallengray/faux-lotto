fb80028e74c7fe790eaa9f5112a76eddc392f65e
Logan Gray <loganallengray@gmail.com>
2023-01-31 11:51:48 -0600

    basic layout and logic for funds page completed

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom"
+import { Funds } from "./funds/Funds"
 import { Games } from "./games/Games"
 
 export const ApplicationViews = () => {
     return (
         <Routes>
             <Route path="games" element={ <Games /> } />
-            {/* <Route path="funds" element={ <TicketContainer /> } />
-            <Route path="profile" element={ <TicketContainer /> } /> */}
+            <Route path="funds" element={ <Funds /> } />
+            {/* <Route path="profile" element={ <TicketContainer /> } /> */}
         </Routes>
     )
 }
