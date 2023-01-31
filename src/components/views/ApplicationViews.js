0d583948f26d0837b594c3ae509b8543ab5147a1
Logan Gray <loganallengray@gmail.com>
2023-01-31 09:43:47 -0600

    added games page

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -1,5 +1,12 @@
+import { Route, Routes } from "react-router-dom"
+import { Games } from "./games/Games"
+
 export const ApplicationViews = () => {
     return (
-        <div>test</div>
+        <Routes>
+            <Route path="games" element={ <Games /> } />
+            {/* <Route path="funds" element={ <TicketContainer /> } />
+            <Route path="profile" element={ <TicketContainer /> } /> */}
+        </Routes>
     )
 }
---
+++ src/components/views/games/Games.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+
+export const Games = () => {
+    return (
+        <>
+            <h2>Game List</h2>
+            <section>
+                <article>
+                    <Link to=""><h3>Coin Flip</h3></Link>
+                    <p>Call the correct side and you double your money!</p>
+                </article>
+                <article>
+                    <Link to=""><h3>Horse Race</h3></Link>
+                    <p>Choose a horse to bet on, riskier bets have better payout!</p>
+                </article>
+            </section>
+        </>
+    )
+}
