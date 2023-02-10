84ab53021780d910630ea382f861cbc9b0ae9820
Logan Gray <loganallengray@gmail.com>
2023-02-10 15:53:52 -0600

    changed nav and side bar to fixed, added more to home page

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -5,33 +5,36 @@ export const NavBar = () => {
     const navigate = useNavigate()
 
     return (
-        <ul className="navbar">
-            <li 
-                id="navbar__home__container"
-                className="navbar__item navbar__home active">
-                <div
-                    onClick={() => {navigate("/")}}
-                ><p id="navbar__home">Faux Lotto</p></div>
-            </li>
-            <li className="navbar__item active">
-                <Link className="navbar__link" to="/games"><p>Games</p></Link>
-            </li>
-            <li className="navbar__item active">
-                <Link className="navbar__link" to="/funds"><p>Funds</p></Link>
-            </li>
-            <li className="navbar__item active">
-                <Link className="navbar__link" to="/profile"><p>Profile</p></Link>
-            </li>
-            {
-                localStorage.getItem("lotto_user")
+        <>
+            <ul className="navbar">
+                <li 
+                    id="navbar__home__container"
+                    className="navbar__item navbar__home active">
+                    <div
+                        onClick={() => {navigate("/")}}
+                    ><p id="navbar__home">Faux Lotto</p></div>
+                </li>
+                <li className="navbar__item active">
+                    <Link className="navbar__link" to="/games"><p>Games</p></Link>
+                </li>
+                <li className="navbar__item active">
+                    <Link className="navbar__link" to="/funds"><p>Funds</p></Link>
+                </li>
+                <li className="navbar__item active">
+                    <Link className="navbar__link" to="/profile"><p>Profile</p></Link>
+                </li>
+                {
+                    localStorage.getItem("lotto_user")
                     ? <li className="navbar__item navbar__logout">
-                        <Link className="navbar__link" to="" onClick={() => {
-                            localStorage.removeItem("lotto_user")
-                            navigate("/", {replace: true})
-                        }}><p>Logout</p></Link>
-                    </li>
-                    : ""
-            }
-        </ul>
+                            <Link className="navbar__link" to="" onClick={() => {
+                                localStorage.removeItem("lotto_user")
+                                navigate("/", {replace: true})
+                            }}><p>Logout</p></Link>
+                        </li>
+                        : ""
+                    }
+            </ul>
+            <div id="navbar-space"></div>
+        </>
     )
 }
