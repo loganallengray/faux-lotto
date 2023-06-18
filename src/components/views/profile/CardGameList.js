1cc1c064fb83108efcf2504a6aa658c97df30d65
Logan Gray <loganallengray@gmail.com>
2023-06-18 17:35:08 -0500

    ready for applications

--- src/components/views/profile/CardGameList.js
+++ src/components/views/profile/CardGameList.js
@@ -1,11 +1,11 @@
-export const CardGameList = ({cardGame, setCardGames, dealerDecks, playerDecks}) => {
+export const CardGameList = ({ cardGame, setCardGames, dealerDecks, playerDecks }) => {
     const handleDelete = (event) => {
         event.preventDefault()
-    
+
         fetch(`http://localhost:8088/cardGames/${cardGame.id}`, {
             method: "DELETE"
         })
-            .then(() => 
+            .then(() =>
                 fetch(`http://localhost:8088/cardGames?playerId=${cardGame.playerId}&_sort=id&_order=desc`)
                     .then((res) => res.json())
                     .then((data) => setCardGames(data))
@@ -22,7 +22,7 @@ export const CardGameList = ({cardGame, setCardGames, dealerDecks, playerDecks})
         if (cardGame.featured) {
             newGame.featured = false
         }
-    
+
         fetch(`http://localhost:8088/cardGames/${cardGame.id}`, {
             method: "PATCH",
             headers: {
@@ -30,7 +30,7 @@ export const CardGameList = ({cardGame, setCardGames, dealerDecks, playerDecks})
             },
             body: JSON.stringify(newGame),
         })
-            .then(() => 
+            .then(() =>
                 fetch(`http://localhost:8088/cardGames?playerId=${cardGame.playerId}&_sort=id&_order=desc`)
                     .then((res) => res.json())
                     .then((data) => setCardGames(data))
@@ -38,61 +38,69 @@ export const CardGameList = ({cardGame, setCardGames, dealerDecks, playerDecks})
     }
 
     return (cardGame.win ? (
-        <li key={cardGame.id} id={`card-game-${cardGame.id}`} class="player--game game--won">
+        <li key={cardGame.id} id={`card-game-${cardGame.id}`} className="player--game game--won">
             <h4>Win</h4>
             <h5>Dealer's Hand</h5>
-            <div class="summ-deck-container">
-                {dealerDecks.filter((deck) => deck.cardGameId === cardGame.id).map((card) => { return (
-                    <div key={card.id} class="summ-card-container">
-                                <img src={`cards/${card?.card?.name}.png`} />    
-                            </div>
-                        )})}
+            <div className="summ-deck-container">
+                {dealerDecks.filter((deck) => deck.cardGameId === cardGame.id).map((card) => {
+                    return (
+                        <div key={card.id} className="summ-card-container">
+                            <img src={`cards/${card?.card?.name}.png`} />
+                        </div>
+                    )
+                })}
             </div>
             <h5>Your Hand</h5>
-            <div class="summ-deck-container">
-                {playerDecks.filter((deck) => deck.cardGameId === cardGame.id).map((card) => { return (
-                    <div key={card.id} class="summ-card-container">
-                                <img src={`cards/${card?.card?.name}.png`} />    
-                            </div>
-                        )})}
+            <div className="summ-deck-container">
+                {playerDecks.filter((deck) => deck.cardGameId === cardGame.id).map((card) => {
+                    return (
+                        <div key={card.id} className="summ-card-container">
+                            <img src={`cards/${card?.card?.name}.png`} />
+                        </div>
+                    )
+                })}
             </div>
             <div>Amount Gained: ${parseFloat(cardGame.newAmount).toFixed(2)}</div>
             <div>{cardGame.date}</div>
-            <div class="game--delete--container">
-                {cardGame.featured ? (<button class="game--unfeature" onClick={(event) => handleFeature(event)}>Unfeature</button>)
-                : (<button class="game--feature" onClick={(event) => handleFeature(event)}>Feature</button>)}
-                <button 
-                    class="game--delete"
+            <div className="game--delete--container">
+                {cardGame.featured ? (<button className="game--unfeature" onClick={(event) => handleFeature(event)}>Unfeature</button>)
+                    : (<button className="game--feature" onClick={(event) => handleFeature(event)}>Feature</button>)}
+                <button
+                    className="game--delete"
                     onClick={(event) => handleDelete(event)}
                 >Delete</button>
             </div>
         </li>
     ) : (
-        <li key={cardGame.id} id={`card-game-${cardGame.id}`} class="player--game game--lost">
+        <li key={cardGame.id} id={`card-game-${cardGame.id}`} className="player--game game--lost">
             <h4>Loss</h4>
             <h5>Dealer's Hand</h5>
-            <div class="summ-deck-container">
-                {dealerDecks.filter((deck) => deck.cardGameId === cardGame.id).map((card) => { return (
-                    <div key={card.id} class="summ-card-container">
-                                <img src={`cards/${card?.card?.name}.png`} />    
-                            </div>
-                        )})}
+            <div className="summ-deck-container">
+                {dealerDecks.filter((deck) => deck.cardGameId === cardGame.id).map((card) => {
+                    return (
+                        <div key={card.id} className="summ-card-container">
+                            <img src={`cards/${card?.card?.name}.png`} />
+                        </div>
+                    )
+                })}
             </div>
             <h5>Your Hand</h5>
-            <div class="summ-deck-container">
-                {playerDecks.filter((deck) => deck.cardGameId === cardGame.id).map((card) => { return (
-                    <div key={card.id} class="summ-card-container">
-                                <img src={`cards/${card?.card?.name}.png`} />    
-                            </div>
-                        )})}
+            <div className="summ-deck-container">
+                {playerDecks.filter((deck) => deck.cardGameId === cardGame.id).map((card) => {
+                    return (
+                        <div key={card.id} className="summ-card-container">
+                            <img src={`cards/${card?.card?.name}.png`} />
+                        </div>
+                    )
+                })}
             </div>
             <div>Amount Lost: ${parseFloat(cardGame.amountBet).toFixed(2)}</div>
             <div>{cardGame.date}</div>
-            <div class="game--delete--container">
-            {cardGame.featured ? (<button class="game--unfeature" onClick={(event) => handleFeature(event)}>Unfeature</button>)
-                : (<button class="game--feature" onClick={(event) => handleFeature(event)}>Feature</button>)}
-                <button 
-                    class="game--delete"
+            <div className="game--delete--container">
+                {cardGame.featured ? (<button className="game--unfeature" onClick={(event) => handleFeature(event)}>Unfeature</button>)
+                    : (<button className="game--feature" onClick={(event) => handleFeature(event)}>Feature</button>)}
+                <button
+                    className="game--delete"
                     onClick={(event) => handleDelete(event)}
                 >Delete</button>
             </div>
